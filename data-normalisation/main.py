44510b4f51047cb29f98f371fe6741d0691dc08a
Tomas Neubauer <tomas+quixers@quix.io>
2026-03-10 12:34:24 +0000

    Edited 'main.py'

--- data-normalisation/main.py
+++ data-normalisation/main.py
@@ -35,13 +35,7 @@ def main():
 
     sdf = sdf.apply(transoform_value_to_row)
 
-    sdf_print = sdf[sdf.contains("accelerometer-x")]
-    sdf_print = sdf_print.apply(lambda row: {
-      "time": row["time"],
-      "acc_x": row["accelerometer-x"],
-      "acc_y": row["accelerometer-y"]
-    })
-    sdf_print.print_table(metadata=False)
+    sdf[sdf.contains("accelerometer-x")].print_table(metadata=False, live_slowdown=0)
 
     # Finish off by writing to the final result to the output topic
     #sdf.to_topic(output_topic)
